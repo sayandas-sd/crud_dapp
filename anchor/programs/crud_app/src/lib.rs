83dd7402689d44bcd044fbcf321d35a79cbbb389
sayandas-sd <sayandasmac@gmail.com>
2024-11-01 01:15:47 +0530

    added create an account

--- anchor/programs/crud_app/src/lib.rs
+++ anchor/programs/crud_app/src/lib.rs
@@ -8,11 +8,46 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 pub mod crud_app {
     use super::*;
 
+    pub fn create_todolist(ctx: Context<CreateState>, title: String, message: String) -> Result<()> {
+        let todo_enrty = &mut ctx.accounts.todo_entry;
+        todo_enrty.owner = *ctx.accounts.owner.key;
+        todo_enrty.title = title;
+        todo_enrty.message = message;
+
+        Ok(())
+    }
+
    
 }
 
 //create
 
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateState<'info> {
+  #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    space = 8 + EntryState::INIT_SPACE,
+    payer = owner,
+  )]
+  pub todo_entry: Account<'info, EntryState>,
+
+  #[account(mut)]
+  pub owner: Signer<'info>,
+  
+  pub system_program: Program<'info, System>,
+}
+ 
 
 
-
+#[account]
+#[derive(InitSpace)]
+pub struct EntryState {
+  pub owner: Pubkey,
+  #[max_len(50)]
+  pub title: String,
+  #[max_len(150)]
+  pub message: String,
+}
