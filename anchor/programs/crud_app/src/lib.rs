82fe3ae64f829d2638abb19a8ecc49243a7b12a3
sayandas-sd <sayandasmac@gmail.com>
2024-11-01 01:16:31 +0530

    update an account

--- anchor/programs/crud_app/src/lib.rs
+++ anchor/programs/crud_app/src/lib.rs
@@ -17,11 +17,15 @@ pub mod crud_app {
         Ok(())
     }
 
-   
+    pub fn update_todolist(ctx: Context<CreateState>, title: String, message: String) -> Result<()> {
+      let todo_entry = &mut ctx.accounts.todo_entry;
+      todo_entry.message = message;
+
+      Ok(())
+    }
 }
 
 //create
-
 #[derive(Accounts)]
 #[instruction(title: String)]
 pub struct CreateState<'info> {
@@ -40,7 +44,29 @@ pub struct CreateState<'info> {
   pub system_program: Program<'info, System>,
 }
  
+//update
 
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateState<'info> {
+  #[account(
+    mut,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    realloc = 8 + EntryState::INIT_SPACE,
+    realloc::payer = owner,
+    realloc::zero =  true,
+  )]
+
+  pub todo_entry: Account<'info, EntryState>,
+
+  #[account(mut)]
+
+  pub owner: Signer<'info>,
+
+  pub system_program:  Program<'info, System>
+
+}
 
 #[account]
 #[derive(InitSpace)]
